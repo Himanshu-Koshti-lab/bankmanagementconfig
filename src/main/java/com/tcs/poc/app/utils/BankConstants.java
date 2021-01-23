4409c6d4c5a0c90dbc425f8c71564b1c6d34a2fe
Himanshu Koshti <Himfeedback74@gmail.com>
2021-01-23 23:07:55 +0530

    Add Constants localhost

---
+++ src/main/java/com/tcs/poc/app/utils/BankConstants.java
@@ -0,0 +1,8 @@
+package com.tcs.poc.app.utils;
+
+public class BankConstants {
+	public static final String ACCOUNT_API_URL = "http://localhost:8084";
+	public static final String DASHBOARD_API_URL = "http://localhost:8083";
+	public static final String USER_API_URL = "http://localhost:8081";
+	public static final String TRANSACTION_API_URL = "http://localhost:8082";
+}
