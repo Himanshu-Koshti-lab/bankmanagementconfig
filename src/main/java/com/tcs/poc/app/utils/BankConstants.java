81f255bdef4a5ad0e17eb150308cd298b4f7610a
Himanshu Koshti <64060564+Himanshu-Koshti-lab@users.noreply.github.com>
2021-07-16 00:00:25 +0530

    Port change

--- src/main/java/com/tcs/poc/app/utils/BankConstants.java
+++ src/main/java/com/tcs/poc/app/utils/BankConstants.java
@@ -6,15 +6,15 @@ public class BankConstants {
 public static final String AWS_URL ="http://ec2-13-59-244-199.us-east-2.compute.amazonaws.com";
 	
 //	For AWS
-	public static final String ACCOUNT_API_URL = AWS_URL + ":8084";
-	public static final String DASHBOARD_API_URL = AWS_URL + ":8083";
-	public static final String USER_API_URL = AWS_URL + ":8081";
-	public static final String TRANSACTION_API_URL = AWS_URL + ":8082";
+//	public static final String ACCOUNT_API_URL = AWS_URL + ":8084";
+//	public static final String DASHBOARD_API_URL = AWS_URL + ":8083";
+//	public static final String USER_API_URL = AWS_URL + ":8081";
+//	public static final String TRANSACTION_API_URL = AWS_URL + ":8082";
 	
 	
 	
-//	public static final String ACCOUNT_API_URL = "http://localhost:8084";
-//	public static final String DASHBOARD_API_URL = "http://localhost:8083";
-//	public static final String USER_API_URL = "http://localhost:8081";
-//	public static final String TRANSACTION_API_URL = "http://localhost:8082";
+	public static final String ACCOUNT_API_URL = "http://localhost:8084";
+	public static final String DASHBOARD_API_URL = "http://localhost:8083";
+	public static final String USER_API_URL = "http://localhost:8081";
+	public static final String TRANSACTION_API_URL = "http://localhost:8082";
 }
